09ce2c6a9222a21bd7e2621be5eee5d95b2efa98
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-01-12 06:56:57 +0700

    membuat data class dan fungsi MahasiswaEvent

--- app/src/main/java/com/android/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/android/firebase/ui/viewmodel/InsertViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.android.firebase.model.Mahasiswa
 import com.android.firebase.repository.MahasiswaRepository
 
 class InsertViewModel(
@@ -64,3 +65,48 @@ class InsertViewModel(
         uiState = FormState.Idle
     }
 }
+
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent. toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
