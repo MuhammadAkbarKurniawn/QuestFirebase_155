a7d286c944a39fcd87d71e4fbdf5d4c65c748a94
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-01-12 07:08:27 +0700

    membuat fungsi dan tampilan pada InsertView

---
+++ app/src/main/java/com/android/firebase/ui/view/InsertView.kt
@@ -0,0 +1,25 @@
+package com.android.firebase.ui.view
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.firebase.ui.viewmodel.InsertViewModel
+import com.android.firebase.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState // state untuk loading, sukses, error
+    val uiEvent = viewModel.uiEvent // state untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+
+}
