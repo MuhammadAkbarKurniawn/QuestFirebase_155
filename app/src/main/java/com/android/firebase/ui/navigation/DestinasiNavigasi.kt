4a43313468a5de58319016e20fc7cd8141ac522c
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2025-01-12 07:52:08 +0700

    membuat DestinasiNavigasi

---
+++ app/src/main/java/com/android/firebase/ui/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,16 @@
+package com.android.firebase.ui.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "home"
+}
+
+object DestinasiInsert : DestinasiNavigasi{
+    override val route: String = "insert"
+    override val titleRes: String = "insert"
+}
